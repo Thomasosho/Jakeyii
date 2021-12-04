a77da3b65fae80842ab668c04f1ef61aae75071a
Thomasosho <tomasosho@gmail.com>
2021-12-04 08:17:52 +0100

    null

--- resources/views/layouts/pap.blade.php
+++ resources/views/layouts/pap.blade.php
@@ -38,7 +38,7 @@
                 </a>
 
               <ul class="nav navbar-nav navbar-right user-nav">
-                <li class="user-name"><span>{{ Auth::user()->name }}</span></li>
+                <li class="user-name"><span>{{ Auth::user()->name ?? '' }}</span></li>
                   <li class="dropdown avatar-dropdown">
                    <img src="admin/asset/img/avatar.jpg" class="img-circle avatar" alt="user name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"/>
                    <ul class="dropdown-menu user-dropdown">
