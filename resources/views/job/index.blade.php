d9b167ef98ab49a60f2edd48dc9727d6e1352e06
Thomasosho <tomasosho@gmail.com>
2021-12-04 07:51:07 +0100

    update

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,5 +2,298 @@
 
 @section('content')
 
+<!-- start: Content -->
+<div id="content">
+            <div class="col-md-12 padding-0">
+              <div class="col-md-12 padding-0">
+                <div class="col-md-12 padding-0">
+                  <div class="panel box-shadow-none content-header">
+                      <div class="panel-body">
+                        <div class="col-md-12">
+                            <h3 class="animated fadeInLeft">Dashboard</h3>
+                            <p class="animated fadeInDown" style="line-height:.4;">
+                              Welcome To Jakeyii.
+                            </p>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <form action="/job/store" method="post">
+                    <div class="col-md-6">
+                        <div class="box-v5 panel">
+                            <div class="panel-heading padding-0 bg-white border-none">
+                                <textarea placeholder="Describe Job to be created"></textarea>
+                            </div>
+                            <div class="panel-body">
+                            <div class="col-md-12 padding-0">
+                                <div class="col-md-6 col-sm-6 col-xs-6 tool">
+                                <input type="file" name="document" class="form-control">
+                                </div>
+                                <div class="col-md-6 col-sm-6 col-xs-6 padding-0">
+                                <button class="btn btn-round pull-right">
+                                    <span>SEND</span>
+                                    <span class="icon-arrow-right icons"></span>
+                                </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="panel box-v6">
+                     <div class="panel-heading">
+                       <h4>Project Report
+                        <span class="icon-options-vertical icons pull-right"></span>
+                      </h4>
+                     </div>
+                    <div class="panel-body padding-0">
+                      <div class="col-md-12 padding-0">
+                          <div class="col-md-12 padding-0" style="height:127px;">
+                            <div class="col-md-12 col-sm-12 box-v6-content-bg" data-progress="100%">
+                              
+                            </div>
+                            <div class="col-md-12 col-sm-12 col-xs-12 padding-0 box-v6-content">
+                              <div class="col-md-10 col-sm-10 col-xs-10">
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+
+                                <h4>Social Network App</h4>
+                                <p>Sat,October 1st 2029</p>
+                              </div>
+                              <div class="col-md-2 col-sm-2 hidden-xs text-center box-v6-progress">
+                                <h3>100%</h3>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                      <div class="col-md-12 padding-0">
+                          <div class="col-md-12 padding-0" style="height:127px;">
+                            <div class="col-md-12 col-sm-12 box-v6-content-bg" data-progress="20%">
+                              
+                            </div>
+                            <div class="col-md-12 col-sm-12 col-xs-12 padding-0 box-v6-content">
+                              <div class="col-md-10 col-sm-10 col-xs-12">
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+
+                                <h4>Lorem Ipsum</h4>
+                                <p>Sat,October 1st 2029</p>
+                              </div>
+                              <div class="col-md-2 col-sm-2 hidden-xs text-center box-v6-progress">
+                                <h3>20%</h3>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                      <div class="col-md-12 padding-0">
+                          <div class="col-md-12 padding-0" style="height:127px;">
+                            <div class="col-md-12 col-sm-12 box-v6-content-bg" data-progress="56%">
+                              
+                            </div>
+                            <div class="col-md-12 col-sm-12 col-xs-12 padding-0 box-v6-content">
+                              <div class="col-md-10 col-sm-10 col-xs-12">
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+
+                                <h4>Dolor sit amet</h4>
+                                <p>Sat,October 1st 2029</p>
+                              </div>
+                              <div class="col-md-2 col-sm-2 hidden-xs text-center box-v6-progress">
+                                <h3>56%</h3>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                      <div class="col-md-12 padding-0">
+                          <div class="col-md-12 padding-0" style="height:127px;">
+                            <div class="col-md-12 col-sm-12 box-v6-content-bg" data-progress="85%">
+                              
+                            </div>
+                            <div class="col-md-12 col-sm-12 col-xs-12 padding-0 box-v6-content">
+                              <div class="col-md-10 col-sm-10 col-xs-12">
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+
+                                <h4>Magna aliqua</h4>
+                                <p>Sat,October 1st 2029</p>
+                              </div>
+                              <div class="col-md-2 col-sm-2 hidden-xs text-center box-v6-progress">
+                                <h3>85%</h3>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                      <div class="col-md-12 padding-0">
+                          <div class="col-md-12 padding-0" style="height:127px;">
+                            <div class="col-md-12 col-sm-12 box-v6-content-bg" data-progress="93%">
+                              
+                            </div>
+                            <div class="col-md-12 col-sm-12 col-xs-12 padding-0 box-v6-content">
+                              <div class="col-md-10 col-sm-10 col-xs-12">
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+                                <img src="admin/asset/img/avatar.jpg"/>
+
+                                <h4>Sed do eiusmod</h4>
+                                <p>Sat,October 1st 2029</p>
+                              </div>
+                              <div class="col-md-2 col-sm-2 hidden-xs text-center box-v6-progress">
+                                <h3>93%</h3>
+                              </div>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                  <div class="panel money-ex text-center">
+                      <div class="panel-heading"></div>
+                      <div class="panel-body">
+                        <div class="col-md-12">
+                         <div id="multi-axes-chart" style="height:240px;"></div>
+                        </div>
+                        <div class="col-md-12" style="margin-top:20px;">
+                             <div class="col-md-6 col-sm-6 col-xs-12 money-ex-left">
+                                <h1>USD 2.718</h1>
+                              </div>
+                              <div class="col-md-6 col-sm-6 col-xs-12 money-ex-right">
+                                <h1>EUR 0.911</h1>
+                              </div>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="panel">
+                      <div class="panel-heading">
+                        <h4>Ready Stock</h4>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                      </div>
+                      <div class="panel-body">
+                        <div class="col-md-12 responsive-table">
+                            <table class="table table-hover">
+                              <tr>
+                                <th>No</th>
+                                <th>Name</th>
+                                <th>Status</th>
+                                <th></th>
+                              </tr>
+                              <tr>
+                                <td>1</td>
+                                <td>Laptop Acer X291</td>
+                                <td>
+                                  <span class="label label-primary">NEW</span>
+                                </td>
+                                <td>
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      Action
+                                      <span class="icon-arrow-down icons"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="#">Confirm</a></li>
+                                      <li><a href="#">Ignore</a></li>
+                                    </ul>
+                                  </div>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>2</td>
+                                <td>LCD Tv 92Bd</td>
+                                <td><span class="label label-primary">NEW</span></td>
+                                <td>
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      Action
+                                      <span class="icon-arrow-down icons"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="#">Confirm</a></li>
+                                      <li><a href="#">Ignore</a></li>
+                                    </ul>
+                                  </div>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>3</td>
+                                <td>aX Microphone X291</td>
+                                <td><span class="label label-success">SOLD</span></td>
+                                <td>
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      Action
+                                      <span class="icon-arrow-down icons"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="#">Confirm</a></li>
+                                      <li><a href="#">Ignore</a></li>
+                                    </ul>
+                                  </div>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>4</td>
+                                <td>Laptop Aspire E1 -471G</td>
+                                <td><span class="label label-danger">CANCEL</span></td>
+                                <td>
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      Action
+                                      <span class="icon-arrow-down icons"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="#">Confirm</a></li>
+                                      <li><a href="#">Ignore</a></li>
+                                    </ul>
+                                  </div>
+                                </td>
+                              </tr>
+                               <tr>
+                                <td>5</td>
+                                <td>Speaker D82K</td>
+                                <td><span class="label label-success">SOLD</span></td>
+                                <td>
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      Action
+                                      <span class="icon-arrow-down icons"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="#">Confirm</a></li>
+                                      <li><a href="#">Ignore</a></li>
+                                    </ul>
+                                  </div>
+                                </td>
+                              </tr>
+                            </table>
+                            <center>
+                              <div class="btn-group" role="group" aria-label="..." style="margin-top:20px;">
+                                      <button type="button" class="btn">
+                                        <span class="icon-arrow-left icons"></span>
+                                      </button>
+                                      <button type="button" class="btn active">1</button>
+                                      <button type="button" class="btn">2</button>
+                                      <button type="button" class="btn">
+                                        <span class="icon-arrow-right icons"></span>
+                                      </button>
+                                    </div>
+                            </center>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+              </div>
+            </div>
+          </div>
+          <!-- end: Content -->
 
 @endsection
